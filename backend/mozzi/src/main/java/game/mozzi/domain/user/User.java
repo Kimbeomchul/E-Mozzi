3c1b4cf683179db9aa5c13d8f2a6ae50632bf995
myoungjinkang <myoungjinkang@kyobobook.com>
2022-07-12 10:01:39 +0900

    수정일자, 생성일자 추가

--- backend/mozzi/src/main/java/game/mozzi/domain/user/User.java
+++ backend/mozzi/src/main/java/game/mozzi/domain/user/User.java
@@ -1,12 +1,12 @@
 package game.mozzi.domain.user;
 
+import game.mozzi.domain.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 // table 생성됨 ORM OBject 기준으로 테이블 만들어짐
 @Builder
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Data
 @Entity // DB에 테이블을 생성 해줌
-public class User {
+public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //번호 증가 전략이 데이터 베이스를 따라 갑니다.
     private Long id; // 서비스 하는 프로그램이 아니라서 int로도 충분함
@@ -33,14 +33,5 @@ public class User {
 
     @Column(name = "gender")
     private String gender;
-
-    private LocalDateTime createDate;
-
-    //자동으로 들어가게 할꺼여서
-
-    @PrePersist // 디비에 INSERT 되기 직전에 실행됨
-    public void createDate(){
-        this.createDate=LocalDateTime.now();
-    }
 
 }
